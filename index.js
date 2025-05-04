5a49c1ebce8704e8c929a1c2f0d739de12396c96
adrianojrvidal <adriano.jv@hotmail.comm>
2025-05-04 17:53:21 -0300

    feat: implement UI for register screen and route for home screen

---
+++ index.js
@@ -0,0 +1,12 @@
+const onClickLogin = () => {
+  const email = document.getElementById("input-email").value;
+
+  if (email.length < 5 || !email.includes("@")) {
+    alert("E-mail inválido!");
+    return;
+  } 
+
+  localStorage.setItem("@WalletApp:userEmail", email);
+
+  window.open("./src/pages/home/index.html", "_self");
+}
